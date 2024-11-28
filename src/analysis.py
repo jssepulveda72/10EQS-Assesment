ced0a505274ce9efae4b09c05fa70907600c0144
jssepulveda98 <93456441+jssepulveda98@users.noreply.github.com>
2024-11-28 15:17:56 -0500

    First cleaning of the data

---
+++ src/analysis.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
+import numpy as np
+import os
+import re
+
+
+data = pd.read_csv("products.csv")
+data.reset_index
+data.columns = ["our_price",
+                "category",
+                "stock",
+                "stock_treshold",
+                "date",
+                "nan"]
+
+data["our_price"] = data["our_price"].apply(lambda x: float(re.findall("[0-9].*", x)[0]))
+data = data.drop(["date","nan"], axis = 1)
+
+
+print(data.iloc[0])
